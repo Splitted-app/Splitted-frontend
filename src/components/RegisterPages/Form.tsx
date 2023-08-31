7e32e2cef31a436ef245938f0c88b2fcf83c8a8a
Katarzyna Rumanowska <kasia.rumanowska@gmail.com>
2023-08-31 09:06:42 +0200

    Init register page

---
+++ src/components/RegisterPages/Form.tsx
@@ -0,0 +1,10 @@
+import '../../css/RegisterPages/Form.css'
+
+function Form() {
+    return (
+      <div className="form">
+      </div>
+    );
+  }
+  
+  export default Form;
---
+++ src/components/RegisterPages/RegisterPage.tsx
@@ -0,0 +1,14 @@
+import '../../css/RegisterPages/RegisterPage.css'
+import Form from './Form'
+
+function RegisterPage() {
+  return (
+    <div className="register-page">
+      <div className='form'>
+        <Form></Form>
+      </div>
+    </div>
+  );
+}
+
+export default RegisterPage;
