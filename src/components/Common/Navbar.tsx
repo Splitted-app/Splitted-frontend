e9398c41f2513c61c7cf7326337c32cd75a165f4
Katarzyna Rumanowska <kasia.rumanowska@gmail.com>
2023-10-22 00:08:40 +0200

    Add modes panel to navbar

--- src/components/Common/Navbar.tsx
+++ src/components/Common/Navbar.tsx
@@ -8,6 +8,7 @@ import PredictionsIcon from '../../assets/images/predictions.png'
 import GoalsIcon from '../../assets/images/goals.png'
 import SettingsIcon from '../../assets/images/settings.png'
 import LogOutIcon from '../../assets/images/log-out.png'
+import AddNewModeIcon from '../../assets/images/add_new_mode.png'
 
 import { useSetRecoilState } from 'recoil';
 import { LogOutPanelVisibilityState } from '../../atoms/LogOutPanelVisibility';
@@ -18,19 +19,40 @@ function Navbar() {
     return (
       <div className="navbar">
         <div className="title">
-          <NavbarItem name="Splitted" font="CeraPro bold" fontSize="27px" link="/home" icon={SplitIcon}></NavbarItem>
+          <NavbarItem name="Splitted" font="CeraPro bold" fontSize="25px" link="/home" icon={SplitIcon}></NavbarItem>
         </div>
         <div className='table-of-contents'>
-          <NavbarItem name="HomePage" font="CeraPro light" fontSize="19px" link="/home" icon={HomePageIcon}></NavbarItem>
-          <NavbarItem name="Transactions" font="CeraPro light" fontSize="19px" link="/home" icon={TransactionsIcon}></NavbarItem>
-          <NavbarItem name="Insights" font="CeraPro light" fontSize="19px" link="/home"icon={InsightsIcon}></NavbarItem>
-          <NavbarItem name="Predictions" font="CeraPro light" fontSize="19px" link="/home"icon={PredictionsIcon}></NavbarItem>
-          <NavbarItem name="Goals" font="CeraPro light" fontSize="19px" link="/home" icon={GoalsIcon}></NavbarItem>
+          <div className='side-pages'>
+            <NavbarItem name="HomePage" font="CeraPro light" fontSize="17px" link="/home" icon={HomePageIcon}></NavbarItem>
+            <NavbarItem name="Transactions" font="CeraPro light" fontSize="17px" link="/home" icon={TransactionsIcon}></NavbarItem>
+            <NavbarItem name="Insights" font="CeraPro light" fontSize="17px" link="/home"icon={InsightsIcon}></NavbarItem>
+            <NavbarItem name="Predictions" font="CeraPro light" fontSize="17px" link="/home"icon={PredictionsIcon}></NavbarItem>
+            <NavbarItem name="Goals" font="CeraPro light" fontSize="17px" link="/home" icon={GoalsIcon}></NavbarItem>
+          </div>
+          <div className='modes-panel'>
+            <div className='modes-panel-title'>
+              Your modes
+            </div>
+            <div className='your-modes-panel'>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta quas eveniet id, ducimus debitis ipsa voluptatem et vel, similique officia totam a odio rem. Velit laudantium quam accusamus dolore delectus?
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta doloremque dolores quod recusandae natus, quae minima distinctio in placeat. Ut nesciunt voluptate molestiae atque minus fugiat ipsa eveniet eum. Expedita.
+            </div>
+            <button className='add-new-mode-button'>
+              <div className='add-new-mode-button-container'>
+                <div className='add-new-mode-icon'>
+                  <img src={AddNewModeIcon}></img>
+                </div>
+                <div className='add-new-mode-text'>
+                  Add new mode
+                </div>
+              </div>
+            </button>
+          </div>
         </div>
         <div className="navbar-footer">          
-          <NavbarItem name="Settings" font="CeraPro medium" fontSize="22px" link="/settings"icon={SettingsIcon}></NavbarItem>
+          <NavbarItem name="Settings" font="CeraPro medium" fontSize="17px" link="/settings"icon={SettingsIcon}></NavbarItem>
           <div onClick={()=>setLogOutPanelVisibility(true)}>
-            <NavbarItem name="Log Out" font="CeraPro medium" fontSize="22px" link="#" icon={LogOutIcon}></NavbarItem>
+            <NavbarItem name="Log Out" font="CeraPro medium" fontSize="17px" link="#" icon={LogOutIcon}></NavbarItem>
           </div>
         </div>
       </div>
