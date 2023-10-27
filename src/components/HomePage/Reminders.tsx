9ba9c3e49cff0041ce701b286320172a3b5f5ce7
Katarzyna Rumanowska <kasia.rumanowska@gmail.com>
2023-10-27 13:52:07 +0200

    Add icons to reminders panel

--- src/components/HomePage/Reminders.tsx
+++ src/components/HomePage/Reminders.tsx
@@ -1,12 +1,15 @@
 import '../../css/HomePage/Reminders.css'
 import CurrentReminder from './CurrentReminder';
 import NotificationIcon from '../../assets/images/notification-bell.png'
+import UpArrowIcon from '../../assets/images/notif_uparrow.svg'
+import DownArrowIcon from '../../assets/images/notif_downarrow.svg'
 
 
 function Reminders() {
     return (
       <div className="reminders">
         <div className='up-arrow'>
+          <img src={UpArrowIcon}></img>
         </div>
         <div className='content'>
           <div className='icon'>
@@ -17,6 +20,7 @@ function Reminders() {
           </div>
         </div>
         <div className='down-arrow'>
+          <img src={DownArrowIcon}></img>
         </div>
       </div>
     );
