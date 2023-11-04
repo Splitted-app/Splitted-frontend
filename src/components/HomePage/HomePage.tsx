a2873901e865005cb3dae61c13ac7cff5909508a
Katarzyna Rumanowska <kasia.rumanowska@gmail.com>
2023-11-04 20:14:22 +0100

    Clean up home page

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -3,39 +3,20 @@ import SignUpFollowUp from "./SignUpFollowUp";
 import Navbar from "../Common/Navbar"
 import HomePageContent from './HomePageContent';
 import { useEffect} from 'react';
-
-
 import { SignUpFollowUpVisibilityState } from '../../atoms/SignUpFollowUpVisibility';
-import { AddTransactionsPanelVisibilityState } from '../../atoms/AddTransactionsPanelVisbility';
-import { ImportCsvPanelVisibilityState } from '../../atoms/ImportCsvPanelVisbility';
-import { ImportCsvCheckPanelVisibilityState } from '../../atoms/ImportCsvCheckPanelVisibility';
-import { ManualAddTransactionsPanelVisibilityState } from '../../atoms/ManualAddTransactionsPanelVisbility';
 import { MenuIconVisibilityState } from '../../atoms/MenuIconVisibility';
 
 import { useRecoilValue, useSetRecoilState } from 'recoil';
 
 function HomePage() {
-    const addTransactionsPanelVisibility = useRecoilValue(AddTransactionsPanelVisibilityState);
-    const importCsvPanelVisibility = useRecoilValue(ImportCsvPanelVisibilityState)
-    const importCsvCheckPanelVisibility = useRecoilValue(ImportCsvCheckPanelVisibilityState);
-    const manualAddTransactionsPanelVisibility = useRecoilValue(ManualAddTransactionsPanelVisibilityState);
-    // const [logOutConfirmationPanelVisible, setLogOutConfirmationPanelVisible] =  useState(true);
-
     const SignUpFollowUpVisibility = useRecoilValue(SignUpFollowUpVisibilityState);
-
-    const isBlurred = SignUpFollowUpVisibility;
-    // const isDarkened = addTransactionsPanelVisibility || 
-    //                     importCsvPanelVisibility ||
-    //                     manualAddTransactionsPanelVisibility ||
-    //                     importCsvCheckPanelVisibility;
-
     const setMenuIconVisibility = useSetRecoilState(MenuIconVisibilityState);
-
+    const isBlurred = SignUpFollowUpVisibility;
+    
     useEffect(()=>
     {
       setMenuIconVisibility(false);
     },[])
-
 
     return (
       <div>
