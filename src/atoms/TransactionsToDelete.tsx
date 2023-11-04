c1c786d9812f8d808b3637e911f7eab4877f2654
Katarzyna Rumanowska <kasia.rumanowska@gmail.com>
2023-11-04 18:42:14 +0100

    Add functionality of deleting multiple transactions at once

---
+++ src/atoms/TransactionsToDelete.tsx
@@ -0,0 +1,7 @@
+import {atom} from 'recoil';
+
+
+export const TransactionsToDeleteState = atom({
+    key: 'TransactionsToDelete',
+    default: [] ,
+  });
--- src/components/HomePage/TransactionsOverview.tsx
+++ src/components/HomePage/TransactionsOverview.tsx
@@ -79,7 +79,7 @@ function TransactionsOverview() {
               Transactions
             </div>
             <div className='transaction-list-container'>
-              <TransactionList transactions={transactions} shadow={true} showTransactionType={false} showDeleteIcon={true}></TransactionList>
+              <TransactionList transactions={transactions} shadow={true} showTransactionType={false} showDeleteIcon={true} showDeleteTransactionRadioButton={false}></TransactionList>
             </div>
             <div className='add-transaction-button-container'>
               <button className='add-transaction-button' onClick={()=>{setAddTransactionsPanelVisibility(true)}}>+</button>
