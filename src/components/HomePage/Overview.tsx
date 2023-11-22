9909e52189562fbdfcd9b0e039cb52667cf2b980
Katarzyna Rumanowska <kasia.rumanowska@gmail.com>
2023-11-22 21:12:00 +0100

    Add InsightsOverview Panel to Home Page

--- src/components/HomePage/Overview.tsx
+++ src/components/HomePage/Overview.tsx
@@ -1,20 +1,83 @@
 import '../../css/HomePage/Overview.css'
 
+import { useState } from 'react';
+
+import DateDisplay from './DateDisplay';
+import DateRangeSelector from './DateRangeSelector';
 import InsightsOverview from './InsightsOverview';
-import PredictionsOverview from './PredictionsOverview';
 import TransactionsOverview from './TransactionsOverview';
 
+
+import { changeDay, changeMonth, changeWeek } from '../../utils';
+
+import leftarrow from '../../assets/images/leftarrow.svg';
+import rightarrow from '../../assets/images/rightarrow.svg';
 interface OverviewInterface
 {
     typeId : number
 }
 
 function Overview({typeId} : OverviewInterface) {
+  const [timeScale, setTimeScale] = useState("daily");
+  let currentDate : Date = new Date();
+  const [dateRange, setDateRange] = useState<any>([{
+    startDate: new Date(),
+    endDate: new Date(),
+    key: 'selection'
+  }]);
+
+  function handleArrow(direction : number)
+  {
+    switch(timeScale)
+    {
+      case "daily":
+        setDateRange([{...dateRange[0], 
+          startDate: changeDay(dateRange[0].startDate, direction),
+          endDate: changeDay(dateRange[0].endDate, direction)
+        }])
+        break;
+      case "weekly":
+        setDateRange([{...dateRange[0], 
+          startDate: changeWeek(dateRange[0].startDate, direction),
+          endDate: changeWeek(dateRange[0].endDate, direction)
+        }])
+        break;
+      case "monthly":
+        setDateRange([{...dateRange[0], 
+          startDate: changeMonth(dateRange[0].startDate, direction),
+          endDate: changeMonth(dateRange[0].endDate, direction)
+        }])
+        break;
+      case "custom":
+        break;
+    }
+  }
     return (
       <div className="overview">
-        {typeId == 0 && <TransactionsOverview></TransactionsOverview>}
-        {typeId == 1 && <InsightsOverview></InsightsOverview>}
-        {typeId == 2 && <PredictionsOverview></PredictionsOverview>}
+        <div className='time-scale-selector-container'>
+            <DateRangeSelector 
+            currentDate={currentDate} 
+            dateRange={dateRange}
+            setDateRange={setDateRange}
+            setTimeScale={setTimeScale}></DateRangeSelector>
+          </div>
+          <div className='simple-time-span-selector-container'>
+            <div className='left-arrow'>
+              <button className='arrow-button' onClick={() => handleArrow(-1)}>
+                <img src={leftarrow} className='navigation-arrow'/>
+              </button>
+            </div>
+              <DateDisplay timeScale={timeScale} date={dateRange[0].startDate}></DateDisplay>
+            <div className='right-arrow'>
+              <button className='arrow-button' onClick={() => handleArrow(1)}>
+                <img src={rightarrow} className='navigation-arrow'/> 
+              </button>
+            </div>
+          </div>
+          <div className='overview-content'>
+            {typeId == 0 && <TransactionsOverview dateRange={dateRange}></TransactionsOverview>}
+            {typeId == 1 && <InsightsOverview dateRange={dateRange}></InsightsOverview>}
+          </div>
       </div>
     );
   }
