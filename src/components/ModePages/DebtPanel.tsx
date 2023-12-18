9d2f45db55bf8c3c533ece5bfb010d076dca92a0
Katarzyna Rumanowska <kasia.rumanowska@gmail.com>
2023-12-18 18:14:13 +0100

    Fix debt currency not showing bug

--- src/components/ModePages/DebtPanel.tsx
+++ src/components/ModePages/DebtPanel.tsx
@@ -16,7 +16,7 @@ function DebtPanel({amount}: DebtPanelInterface) {
         <div className='debt-panel-title'>
             Debt:
         </div>
-        <div className='debt-panel-main-content' style={{color:(amount<0)? '#A30D0D': '#35B736'}}>
+        <div className='debt-panel-main-content' style={{color:(amount<0)? '#A30D0D': '#18B470'}}>
             <div className='debt-panel-amount'>
                 {amount}
             </div>
