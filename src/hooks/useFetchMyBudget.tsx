1fff885e4c8d61ce429a7b7ae2981623bb73cfbe
Katarzyna Rumanowska <kasia.rumanowska@gmail.com>
2023-12-30 12:42:11 +0100

    Remove caching budget

--- src/hooks/useFetchMyBudget.tsx
+++ src/hooks/useFetchMyBudget.tsx
@@ -2,13 +2,16 @@ import { useEffect, useState } from "react";
 
 import { useRecoilState } from "recoil";
 
-import api from "../services/api";
 import { MyBudgetUpdaterState } from "../atoms/MyBudgetUpdater";
+import { TransactionUpdaterState } from "../atoms/TransactionUpdater";
+
+import api from "../services/api";
+
 
 
 export default function useFetchMyBudget() {
     const [myBudgetUpdater, setMyBudgetUpdater] = useRecoilState(MyBudgetUpdaterState);
-    let isCached = false;
+    const [transactionUpdater, setTransactionUpdater] = useRecoilState(TransactionUpdaterState);
     const [data, setData] = useState({
         "id": "",
         "name": "",
@@ -21,16 +24,12 @@ export default function useFetchMyBudget() {
     const [error, setError] = useState<boolean>(false);
 
     useEffect(() => {
-        if (isCached && !myBudgetUpdater)
-            return;
-
         setLoading(true);
         api.get('/api/users/budgets?budgetType=Personal,Family')
         .then((res) => {
             setError(false);
             setData(res.data[0])
             setMyBudgetUpdater(false);
-            isCached = true;
         })
         .catch(error => {
             setError(true);
@@ -39,7 +38,7 @@ export default function useFetchMyBudget() {
         .finally(()=>{
             setLoading(false);
         })
-    }, [myBudgetUpdater]) // todo update when user switches to family budget
+    }, [myBudgetUpdater, transactionUpdater])
 
     return {data, loading, error};
 }
